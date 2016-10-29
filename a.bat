bf167fb3ab4f633cfb561e41157bea111be0b569
Arthursavage <alerc@meta.ua>
2016-10-29 20:19:12 +0300

    Update

--- a.bat
+++ a.bat
@@ -3,5 +3,5 @@ git init
 git remote add origin https://gitart:Gerda3000@github.com/Gitart/systems.git
 git add -A 
 git commit -m "Update"
-git push  -U origin master
+git push  origin master
 pause
